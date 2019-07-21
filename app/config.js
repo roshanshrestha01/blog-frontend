aaea68e5823f8a26f37e78448f3b6f64187c80a2
Roshan Shrestha <roshanshrestha01@gmail.com>
2019-07-21 10:30:14 +0545

    Settin API_URL from scripts

--- app/config.js
+++ app/config.js
@@ -1,3 +1,3 @@
 export default {
-  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000/v1' : 'https://powerful-citadel-52674.herokuapp.com/v1',
+  baseURL: `${process.env.API_URL}/v1`
 };
--- config/webpack.base.babel.js
+++ config/webpack.base.babel.js
@@ -74,7 +74,8 @@ module.exports = (options) => ({
     // drop any unreachable code.
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        API_URL: JSON.stringify(process.env.API_URL)
       }
     })
   ]),
